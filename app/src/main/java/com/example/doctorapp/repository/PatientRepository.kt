2d1fd01461a0499a89bcee00e2cd6f37ae438846
Bing <bpan2@users.noreply.github.com>
2022-12-06 14:17:52 -0500

    Solved inserting data into dbs after retrieving it from our microservice. The data flow from our microservice to our app is successfully implemented.

--- app/src/main/java/com/example/doctorapp/repository/PatientRepository.kt
+++ app/src/main/java/com/example/doctorapp/repository/PatientRepository.kt
@@ -19,41 +19,44 @@ class PatientRepository(application: Application) {
     var searchResults = MutableLiveData<List<PatientEntity>>()
     var db: AppDatabase? = null
 
-    private var patientDao: PatientDao?
+
     private val coroutineScope = CoroutineScope(Dispatchers.Main)
 
     init {
         db = AppDatabase.getInstance(application)
-        patientDao = db?.patientDao()
-        allPatients = patientDao?.getAllPatients()
+        allPatients = db?.patientDao()?.getAllPatients()
 
-        coroutineScope.launch{
-            val patientList = PatientService.retrofitClient.getAllPatients()
-            patientDao?.insertAll(patientList)
-        }
-
-       /* //if the local dbs is empty, retrieve data from the remote sources
-        if (allPatients == null ){
+        //if the local dbs is empty, retrieve data from the remote sources
+        if (allPatients?.value == null) {
             Log.i("inside repo init", "allPatients is null branch")
-            coroutineScope.launch{
+            coroutineScope.launch {
                 val patientList = PatientService.retrofitClient.getAllPatients()
-                patientDao?.insertAll(patientList)
+                withContext(Dispatchers.IO) {
+                    db?.patientDao()?.insertAll(patientList)
+                }
             }
-        }*/
+        }
     }
 
 
     //operations on a single patient
-    suspend fun insertPatient(patientEntity: PatientEntity) = db?.patientDao()?.insertPatient(patientEntity)
+    suspend fun insertPatient(patientEntity: PatientEntity) =
+        db?.patientDao()?.insertPatient(patientEntity)
+
     suspend fun getPatientById(id: Int) = db?.patientDao()?.getPatientById(id)
-    suspend fun updatePatient(patientEntity: PatientEntity) = db?.patientDao()?.updatePatient(patientEntity)
+    suspend fun updatePatient(patientEntity: PatientEntity) =
+        db?.patientDao()?.updatePatient(patientEntity)
+
     suspend fun deletePatientById(id: Int) = db?.patientDao()?.deletePatientById(id)
     suspend fun deletePatient(patient: PatientEntity) = db?.patientDao()?.deletePatient(patient)
 
 
     //operations on multiple or all patients
     suspend fun insertAll(patients: List<PatientEntity>) = db?.patientDao()?.insertAll(patients)
-    suspend fun getAllPatients(): LiveData<List<PatientEntity>>? = db?.patientDao()?.getAllPatients()
+    suspend fun getAllPatients(): LiveData<List<PatientEntity>>? =
+        db?.patientDao()?.getAllPatients()
+
     suspend fun deleteAllPatients() = db?.patientDao()?.deleteAllPatients()
-    suspend fun deletePatients(selectedPatients: List<PatientEntity>) = db?.patientDao()?.deletePatients(selectedPatients)
+    suspend fun deletePatients(selectedPatients: List<PatientEntity>) =
+        db?.patientDao()?.deletePatients(selectedPatients)
 }
